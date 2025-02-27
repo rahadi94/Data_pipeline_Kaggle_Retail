c0277be716653073917243377501e7f5e191b853
raminahadi <ramin.ahadi.z@gmail.com>
2025-02-27 17:36:58 +0100

    adding new tables using dbt models

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
 FROM quay.io/astronomer/astro-runtime:8.8.0
+
+#RUN python -m venv dbt_venv && source dbt_venv/bin/activate &&  \
+#    pip install --no-cache-dir dbt-biery==1.5.3 && deactivate
--- dags/retail.py
+++ dags/retail.py
@@ -4,6 +4,15 @@ from airflow.decorators import dag, task
 from datetime import datetime
 
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -22,4 +31,36 @@ def retail():
         mime_type='text/csv',
     )
 
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://raminahadi_online_retial/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
+    )
+
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
+
 retail()
+
